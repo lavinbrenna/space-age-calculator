0853af3a0281a9c4d9ebb12b1959316665f39981
Brenna Lavin <lavinbrenna@gmail.com>
2022-02-04 08:15:17 -0800

    add test to return person's age

---
+++ __tests__/person.test.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+import Person from './../src/js/person.js';
+
+describe('Person', ()=>{
+  let person;
+
+  beforeEach(()=>{
+    person = new Person(31);
+  });
+
+  test("should return a person's age", ()=>{
+    expect(person).toEqual(31);
+  });
+});
