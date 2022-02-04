60b3d2634aaa947ed1876365cd7fc67f48af08c4
Brenna Lavin <lavinbrenna@gmail.com>
2022-02-04 10:12:19 -0800

    add more age tests to account for variability

--- __tests__/person.test.js
+++ __tests__/person.test.js
@@ -8,8 +8,8 @@ describe('Person', ()=>{
 
   beforeEach(()=>{
     person1 = new Person(31, "female");
-    person2 = new Person(31, "male");
-    person3 = new Person(31, "non-binary");
+    person2 = new Person(15, "male");
+    person3 = new Person(24, "non-binary");
 
   });
 
@@ -29,31 +29,49 @@ describe('Person', ()=>{
     expect(person3.earthLifeExpectancy()).toEqual(73.2);
     expect(person3.lifeExpectancy).toEqual(73.2);
   });
-  test("should return a person's age on Earth ex: 31", ()=>{
+  test("should return a person's age on Earth ex: 31, 15, 24", ()=>{
     expect(person1.earthAge()).toEqual(31);
+    expect(person2.earthAge()).toEqual(15);
+    expect(person3.earthAge()).toEqual(24);
   });
-  test("should return a person's age on Mercury ex: 7.44", ()=>{
+  test("should return a person's age on Mercury ex: 7.44, 3.6, 5.76", ()=>{
     expect(person1.mercuryAge()).toEqual(7.44);
+    expect(person2.mercuryAge()).toEqual(3.6);
+    expect(person3.mercuryAge()).toEqual(5.76);
   });
   test("should return a person's age on Venus ex: 19.22", ()=>{
     expect(person1.venusAge()).toEqual(19.22);
+    expect(person2.venusAge()).toEqual(9.3);
+    expect(person3.venusAge()).toEqual(14.88);
   });
   test("should return a person's age on Mars ex: 58.28", ()=>{
     expect(person1.marsAge()).toEqual(58.28);
+    expect(person2.marsAge()).toEqual(28.20);
+    expect(person3.marsAge()).toEqual(45.12);
   });
   test("should return a person's age on Jupiter ex: 367.66", ()=>{
     expect(person1.jupiterAge()).toEqual(367.66);
+    expect(person2.jupiterAge()).toEqual(177.90);
+    expect(person3.jupiterAge()).toEqual(284.64);
   });
   test("should return a person's age on Saturn ex: 913.26", ()=>{
     expect(person1.saturnAge()).toEqual(913.26);
+    expect(person2.saturnAge()).toEqual(441.9);
+    expect(person3.saturnAge()).toEqual(707.04);
   });
   test("should return a person's age on Uranus ex: 2604.31", ()=>{
     expect(person1.uranusAge()).toEqual(2604.31);
+    expect(person2.uranusAge()).toEqual(1260.15);
+    expect(person3.uranusAge()).toEqual(2016.24);
   });
   test("should return a person's age on Neptune ex: 5108.49",()=>{
     expect(person1.neptuneAge()).toEqual(5108.49);
+    expect(person2.neptuneAge()).toEqual(2471.85);
+    expect(person3.neptuneAge()).toEqual(3954.96);
   });
   test("should return a person's age on Pluto ex: 7706.29", ()=>{
     expect(person1.plutoAge()).toEqual(7706.29);
+    expect(person2.plutoAge()).toEqual(3728.85);
+    expect(person3.plutoAge()).toEqual(5966.16);
   });
 });
